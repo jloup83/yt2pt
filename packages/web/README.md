40e2e8cb2e470bdee831fc32b72d72f6a478bb9d
Jean-Loup TAILLEFUMIRE <taillefumire.jloup@icloud.com>
2026-04-19 00:27:07 +0800

    Tabula Rasa

--- packages/web/README.md
+++
@@ -1,3 +0,0 @@
-# @yt2pt/web
-
-Vue 3 + Vite Web UI for yt2pt. To be scaffolded in #63.
--- packages/web/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "@picocss/pico/css/pico.min.css";
-import App from "./App.vue";
-import { router } from "./router";
-
-createApp(App).use(router).mount("#app");
